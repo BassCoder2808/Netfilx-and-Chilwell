c98fa57ed397c7b74aa672be8b5d9d5a64149771
BassCoder2808 <vedant.jolly@spit.ac.in>
2023-02-09 22:34:37 +0530

    Added mapping for each Prem team

---
+++ pepai/framework/mappings.py
@@ -0,0 +1,61 @@
+"""
+map between different conventions used in different data sources.
+"""
+
+
+positions = {1: "GK", 2: "DEF", 3: "MID", 4: "FWD"}
+
+alternative_team_names = {
+    "ARS": ["1", "Arsenal", "Arsenal FC"],
+    "AVL": ["2", "Aston Villa", "Aston Villa FC"],
+    "BOU": ["3", "Bournemouth", "AFC Bournemouth"],
+    "BRE": ["4", "Brentford", "Brentford FC"],
+    "BHA": [
+        "5",
+        "Brighton and Hove Albion",
+        "Brighton",
+        "Brighton & Hove Albion FC",
+        "Brighton & Hove Albion",
+    ],
+    "CHE": ["6", "Chelsea", "Chelsea FC"],
+    "CRY": ["7", "Crystal Palace", "Crystal Palace FC"],
+    "EVE": ["8", "Everton", "Everton FC"],
+    "FUL": ["9", "Fulham", "Fulham FC"],
+    "LEI": ["10", "Leicester City", "Leicester", "Leicester City FC"],
+    "LEE": ["11", "Leeds", "Leeds United", "Leeds Utd"],
+    "LIV": ["12", "Liverpool", "Liverpool FC"],
+    "MCI": ["13", "Manchester City", "Man City", "Manchester City FC"],
+    "MUN": [
+        "14",
+        "Manchester United",
+        "Man Utd",
+        "Manchester United FC",
+        "Manchester Utd",
+    ],
+    "NEW": [
+        "15",
+        "Newcastle United",
+        "Newcastle",
+        "Newcastle United FC",
+        "Newcastle Utd",
+    ],
+    "NFO": ["16", "Nott'm Forest", "Nottingham Forest"],
+    "SOU": ["17", "Southampton", "Southampton FC"],
+    "TOT": ["18", "Tottenham Hotspur", "Spurs", "Tottenham Hotspur FC"],
+    "WHU": ["19", "West Ham United", "West Ham", "West Ham United FC"],
+    "WOL": ["20", "Wolves", "Wolverhampton Wanderers", "Wolverhampton Wanderers FC"],
+    "BUR": ["Burnley", "Burnley FC"],
+    "NOR": ["Norwich City", "Norwich"],
+    "WAT": ["Watford", "Watford FC"],
+    "SHU": ["Sheffield United", "Sheffield Utd"],
+    "WBA": ["West Bromwich Albion", "West Brom"],
+    "SUN": ["Sunderland"],
+    "SWA": ["Swansea City"],
+    "STK": ["Stoke City", "Stoke"],
+    "MID": ["Middlesbrough"],
+    "HUL": ["Hull", "Hull City"],
+    "QPR": ["Queens Park Rangers"],
+    "CAR": ["Cardiff", "Cardiff City", "Cardiff City FC"],
+    "HUD": ["Huddersfield Town", "Huddersfield", "Huddersfield Town AFC"],
+    "DER": ["Derby", "Derby County"],
+}
